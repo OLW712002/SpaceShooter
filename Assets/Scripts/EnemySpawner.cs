ed9b739917b3b6081c4a6dc9a1db93ad37e9cb03
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2024-12-21 11:23:09 +0700

    Update EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,14 +6,34 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] WaveConfigSO currentWave;
+    [SerializeField] float delayForNextEnemy = 1.0f;
 
     void Start()
     {
-        SpawnEnemies();
+        //SpawnEnemies();
+        StartCoroutine(SpawnEnemiesWithDelay());
+    }
+
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
     }
 
     private void SpawnEnemies()
     {
+        for (int i = 0; i < currentWave.GetEnemiesCount(); i++)
+        {
+            Instantiate(currentWave.GetEnemyPrefabs(i), currentWave.GetStartingWaypoint().position, Quaternion.identity, transform);
+        }
         
+    }
+
+    IEnumerator SpawnEnemiesWithDelay()
+    {
+        for (int i = 0; i < currentWave.GetEnemiesCount(); i++)
+        {
+            Instantiate(currentWave.GetEnemyPrefabs(i), currentWave.GetStartingWaypoint().position, Quaternion.identity, transform);
+            yield return new WaitForSecondsRealtime(delayForNextEnemy);
+        }
     }
 }
